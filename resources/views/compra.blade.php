0d4b906d4d1ff0e0a18f5883b8e6640aa50c5008
Guillex99 <mf17006@ues.edu.sv>
2021-05-31 16:34:24 -0600

    Cambios el compra.blade.php

--- resources/views/compra.blade.php
+++ resources/views/compra.blade.php
@@ -1,43 +1,51 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <!-- CSS only -->
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
 
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Compra</title>
 </head>
+
 <body>
-    <div class="card-body">
-    <h3 class="text-success text-center">
-                @if(session('info'))
-                    <div class="alert alert-success">
-                    {{session('info')}}
-                    </div>
-                @endif
-    </h3>
-    <table class="table table-hover table-sm">
-        <thead>
-            <th>Articulo:</th>
-            <th>Precio(USD):</th>
-        </thead>
-        <tbody>
-            @foreach($ventas as $venta)
-            <tr>
-                <td>{{$venta->nombre_venta}}</td>
-                <td>{{$venta->precio}}</td>
-            </tr>
-            @endforeach
-        </tbody>
-    </table>
+<div class="card text-md-center">
+    <div class="card-body col-auto">
+        <h3 class="text-success text-center">
+            @if(session('info'))
+            <div class="alert alert-success">
+                {{session('info')}}
+            </div>
+            @endif
+        </h3>
+        <h3 class="text-center text-danger">Historial de compras:</h3>
+        <table class="table table-hover table-sm">
+            <thead>
+                <th>Articulo:</th>
+                <th>Precio(USD):</th>
+            </thead>
+            <tbody>
+                @foreach($ventas as $venta)
+                <tr>
+                    <td>{{$venta->nombre_venta}}</td>
+                    <td>{{$venta->precio}}</td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
 
-    <div class="text-center">
-        <a href="/home"><button type="submit" class="btn btn-warning">Regresar a comprar</button></a>
+        <div class="text-center">
+            <a href="/home"><button type="submit" class="btn btn-warning">Regresar a comprar</button></a>
+        </div>
+    </div>
+</div>
 
-    </div>
-    </div>
+
+
 
 </body>
+
 </html>
